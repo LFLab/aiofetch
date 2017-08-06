26f32f6041a107b1b9f869054e32ff4404dd9af0
lanf0n <lanfon72@gmail.com>
2017-08-07 00:46:43 +0800

    use connector for throtling, add retry feature

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # aiofetch
-(async) comic image fetcher for http://www.cartoonmad.com.
+(async) comic image fetcher for http://www.cartoonmad.com/
 
 # library required:
-  - aiohttp
-  - aiofiles
-  - bs4
+  see [requirements.txt](https://github.com/LFLab/aiofetch/blob/master/requirements.txt)
+
 # usage
 use `python aiofetch.py -h` to get usage.  something like that:
 ```
 python aiofetch.py http://www.cartoonmad.com/comic/1300.html -d bleach -l 100
 ```
+
+# performance
+```
+$ python aiofetch.py http://www.cartoonmad.com/comic/4387.html -d slime > history.log
+=== program start at 2017-08-07 00:37:42.402054 ===
+=== tasks done at 2017-08-07 00:38:30.575810 ===
+retry for 75 page(s) at 2017-08-07 00:38:30.576810
+=== program end at 2017-08-07 00:38:54.491178 ===
+```
+total 781 files, 210MB
--- aiofetch.py
+++ aiofetch.py
@@ -1,74 +1,95 @@
 import os
+import sys
 import asyncio
 import argparse
+from datetime import datetime
 
 import aiohttp
-import async_timeout
 from aiofiles import open as aopen
 from bs4 import BeautifulSoup as bs
 
-__version__ = "0.2.2"
-__doc__ = "comic image fetcher for http://www.cartoonmad.com."
+__version__ = "0.3.3"
+__doc__ = "comic image fetcher for http://www.cartoonmad.com/"
 
 
-async def save_img(fpath, url, semaphore):
-    print("request img from", url)
-    async with semaphore, aiohttp.request("GET", url) as r, aopen(fpath, 'wb') as f:
-        print("saving file to:", fpath, url)
-        await f.write(await r.read())
+async def save_img(fpath, url, session):
+    try:
+        async with session.get(url, timeout=45) as r:
+            print("request img from", url)
+            data = await r.read()
+        async with aopen(fpath, 'wb') as f:
+            print("saving file to:", fpath, url)
+            await f.write(data)
+    except Exception as e:
+        print("fail to save img from", url, "due to", repr(e))
+        return fpath, url
+    else:
+        return None
 
 
-async def fetch_imgs(url, vol):
-    print('request page 1:', url)
-    with async_timeout.timeout(10):
-        async with aiohttp.request("GET", url) as r:
-            soup = bs(await r.text('big5-hkscs'), 'html.parser')
+async def fetch_imgs(url, vol, session):
+    print('request vol %s page 1 in url: %s' % (vol, url))
+    async with session.get(url, timeout=20) as r:
+        soup = bs(await r.text('big5-hkscs'), 'html.parser')
     ps = soup.select('option')[1:]
     img_url = soup.select('img[onload]')[0]['src']
     img_root = img_url.rsplit('/', 1)[0]
     return vol, ("%s/%03d.jpg" % (img_root, idx) for idx, _ in enumerate(ps, 1))
 
 
-async def fetch_vols(url):
-    r = await aiohttp.request('GET', url)
-    soup = bs(await r.text('big5-hkscs'), "html.parser")
+async def fetch_vols(url, session):
+    async with session.get(url, timeout=10) as r:
+        soup = bs(await r.text('big5-hkscs'), "html.parser")
     vols = soup.select('fieldset:nth-of-type(2) a')
     cur = url.rsplit('/', 2)[0]
     return ((v.text, cur + v['href']) for v in vols)
 
 
 async def main(args):
+    print("=== program start at %s ===" % datetime.now(), file=sys.stderr)
     dest = args.dir or "Comic_from_%s" % args.url.split('/', 2)[-1]
     if not os.path.isdir(dest):
         os.makedirs(dest)
     os.chdir(dest)
+    conn = aiohttp.TCPConnector(limit=args.limit)
+    session = aiohttp.ClientSession(connector=conn)
+
     fs = []
-    for vol, url in await fetch_vols(args.url):
+    for vol, url in await fetch_vols(args.url, session):
         if not os.path.isdir(vol):
             os.makedirs(vol)
             print("create dir %s" % vol)
-        fs.append(fetch_imgs(url, vol))
+        fs.append(asyncio.ensure_future(fetch_imgs(url, vol, session)))
 
-    img_fs, semaphore = [], asyncio.Semaphore(args.limit)
+    img_fs = []
     for f in asyncio.as_completed(fs):
         try:
             vol, imgs = await f
         except asyncio.TimeoutError:
-            print("request for %s timeout." % vol)
-            print("skip to next.")
+            print("request for %s timeout. skip to next." % vol, file=sys.stderr)
         else:
             for idx, img_url in enumerate(imgs, 1):
                 fpath = os.path.join(vol, "%03d.jpg" % idx)
-                img_f = asyncio.ensure_future(save_img(fpath, img_url, semaphore))
-                img_fs.append(img_f)
-    await asyncio.wait(img_fs)
+                if not os.path.exists(fpath):
+                    img_f = asyncio.ensure_future(save_img(fpath, img_url, session))
+                    img_fs.append(img_f)
+    done, _ = await asyncio.wait(img_fs)
 
+    print("=== tasks done at %s ===" % datetime.now(), file=sys.stderr)
+    pending = [f.result() for f in done if f.result()]
+    while any(pending):
+        print(" * retry for %s page(s) at %s" % (len(pending), datetime.now()), file=sys.stderr)
+        done, _ = await asyncio.wait([save_img(fp, url, session) for fp, url in pending])
+        pending = [f.result() for f in done if f.result()]
+
+    print("=== program end at %s ===" % datetime.now(), file=sys.stderr)
+    session.close()
 
 if __name__ == '__main__':
     arg = argparse.ArgumentParser(description=__doc__)
     arg.add_argument("url", help="the url.")
     arg.add_argument("-dest", "-d", dest="dir", help="the destnation folder.")
-    arg.add_argument("-limit", "-l", type=int, default=50, dest='limit',
+    arg.add_argument("-limit", "-l", type=int, default=256, dest='limit',
                      help="the concurrent saving image limit.")
 
     loop = asyncio.get_event_loop()
